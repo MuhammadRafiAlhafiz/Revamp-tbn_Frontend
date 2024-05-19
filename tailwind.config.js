00e24905a4e033cfa2a1c49d55c8a17161c9ebc6
MuhammadRafiAlhafiz <rafialhafiiz231002@gmail.com>
2024-05-19 11:29:07 +0700

    Update Week 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    daisyui: {
+      themes: ["light"],
+    },
     extend: {
       colors: {
         hijau: "#C9D32C",
         background: "#FAFBE6",
         biru: "#25396F",
+        graybg: "#D1D2AD",
       },
       fontFamily: {
         popins: ["Poppins, sans-serif"],
+        nunito: ["Nunito, sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
